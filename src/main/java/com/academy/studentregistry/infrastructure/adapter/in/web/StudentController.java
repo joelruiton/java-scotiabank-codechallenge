1c143d3a370d83ee24a2eb874bd5c45297eaed90
Joel Ruitón <joelruiton@gmail.com>
2026-04-27 00:41:20 -0500

    fix: cors issue

--- src/main/java/com/academy/studentregistry/infrastructure/adapter/in/web/StudentController.java
+++ src/main/java/com/academy/studentregistry/infrastructure/adapter/in/web/StudentController.java
@@ -9,18 +9,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/v1/students")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class StudentController {
 
     private final StudentUseCase studentUseCase;
