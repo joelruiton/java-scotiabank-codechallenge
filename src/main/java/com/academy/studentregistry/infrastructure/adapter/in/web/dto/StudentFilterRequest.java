52d56d61322a7959d2d100ddba9911b6f080c066
Joel Ruitón <joelruiton@gmail.com>
2026-05-05 14:51:10 -0500

    fix: filter request

--- src/main/java/com/academy/studentregistry/infrastructure/adapter/in/web/dto/StudentFilterRequest.java
+++ src/main/java/com/academy/studentregistry/infrastructure/adapter/in/web/dto/StudentFilterRequest.java
@@ -1,9 +1,17 @@
 package com.academy.studentregistry.infrastructure.adapter.in.web.dto;
 
+import com.academy.studentregistry.domain.model.Status;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class StudentFilterRequest {
+    @NotBlank(message = "El campo 'estado' es obligatorio")
+    @Pattern(regexp = Status.ACTIVO + "|" + Status.INACTIVO, message = "El campo 'estado' solo acepta: ACTIVO o INACTIVO")
     private String estado;
+
+    @NotNull(message = "El campo 'edad' es obligatorio")
+    @Min(value = 1, message = "El campo 'edad' debe ser minimo 1")
+    @Max(value = 120, message = "El campo 'edad' debe ser maximo 120")
     private Integer edad;
 }
